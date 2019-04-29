952f899f8ee0081a5b2a8e9c8e775dd70fc84dfa
zhangkai <871227769@qq.com>
2019-04-29 10:48:46 +0800

    Update 源.cpp

--- 字节跳动2018校招算法方向（第一批）/1/1/源.cpp
+++ 字节跳动2018校招算法方向（第一批）/1/1/源.cpp
@@ -1,4 +1,6 @@
-//�ô��볬ʱ��ͨ��50%
+/*P为给定的二维平面整数点集。定义 P 中某点x，如果x满足 P 中任意点都不在 x 的右上方区域内（横纵坐标都大于x），则称其为“最大的”。求出所有“最大的”点的集合。（所有点的横坐标和纵坐标都不重复, 坐标轴范围在[0, 1e9) 内）
+如下图：实心点为满足条件的点的集合。请实现代码找到集合 P 中的所有 ”最大“ 点的集合并输出。*/
+//该代码超时，通过50%
 /*#include<iostream>
 #include<cstdio>
 #include<vector>
@@ -160,7 +162,7 @@ int main()
 		{
 			tmp = s.top();
 			s.pop();
-			printf("%d %d\n", tmp.position_x, tmp.position_y);//��Ե�printfҪ���ڲ���caseͨ���ʺ���cout��������
+			printf("%d %d\n", tmp.position_x, tmp.position_y);//麻痹的printf要快于并且case通过率好于cout！！！！
 		}
 	}
 	return 0;
